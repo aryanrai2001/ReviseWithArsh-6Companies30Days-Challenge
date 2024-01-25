7047c5573f634302b6802d55ddb28c7d8fd2945d
Aryan Rai <aryanrai3112@gmail.com>
2024-01-25 18:53:28 +0530

    Day 18 finished!

---
+++ Day 18/Problem 3/LeetCode 524 - Longest Word in Dictionary through Deleting.java
@@ -0,0 +1,19 @@
+class Solution {
+  public String findLongestWord(String s, List<String> d) {
+    String ans = "";
+
+    for (final String word : d)
+      if (isSubsequence(word, s))
+        if (word.length() > ans.length() ||
+            word.length() == ans.length() && word.compareTo(ans) < 0)
+          ans = word;
+    return ans;
+  }
+  private boolean isSubsequence(final String a, final String b) {
+    int i = 0;
+    for (final char c : b.toCharArray())
+      if (i < a.length() && c == a.charAt(i))
+        ++i;
+    return i == a.length();
+  }
+}
